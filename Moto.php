afa93f48a5d980bcd2c6b2b7d71909375d10676a
MawZzzz <mauricarp2002@gmail.com>
2025-04-11 15:02:02 -0300

    inicio Moto

--- Moto.php
+++ Moto.php
@@ -1,5 +1,17 @@
 <?php
+class Moto {
+    private $codigo;
+    private $costo;
+    private $anioFab;
+    private $desc;
+    private $pia;
+    private $activa;
 
+
+    public function __construct($code ,$cost ,$yearF ,$description ,$pai){
+        
+    }
+}
 
 
 
